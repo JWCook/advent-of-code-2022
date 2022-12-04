9a9e3e59e7228f251a341f22569ff52738ba2e4e
Jordan Cook <jordan.cook.git@proton.me>
2022-12-04 15:42:49 -0600

    Problem 3, part 2

--- solutions/problem_3.py
+++ solutions/problem_3.py
@@ -15,7 +15,7 @@ def char_priority(char: str) -> int:
     return ord(char) - offset
 
 
-def get_common_item(line: str) -> str:
+def get_str_common_item(line: str) -> str:
     """Get the character that appears in both halves of a string"""
     i = len(line) // 2
     pack_1 = set(line[:i])
@@ -23,17 +23,38 @@ def get_common_item(line: str) -> str:
     return list(pack_1 & pack_2)[0]
 
 
-def get_total_priority() -> int:
+def get_lines_common_item(*lines: str) -> str:
+    """Get the character that appears in all specified lines"""
+    intersection = set(lines[0])
+    for line in lines[1:]:
+        intersection &= set(line.strip())
+    return list(intersection)[0]
+
+
+def get_pack_total_priority() -> int:
     total_priority = 0
     with open(INPUTS_DIR / 'input_3') as fp:
-        while line := fp.readline().strip():
-            common_item = get_common_item(line)
+        while line := fp.readline():
+            common_item = get_str_common_item(line)
             total_priority += char_priority(common_item)
     return total_priority
 
 
+def get_badge_total_priority() -> int:
+    total_priority = 0
+    with open(INPUTS_DIR / 'input_3') as fp:
+        while True:
+            try:
+                common_item = get_lines_common_item(next(fp), next(fp), next(fp))
+                total_priority += char_priority(common_item)
+            except StopIteration:
+                break
+    return total_priority
+
+
 def main():
-    logger.info(f'Part 1: {get_total_priority()}')
+    logger.info(f'Part 1: {get_pack_total_priority()}')
+    logger.info(f'Part 2: {get_badge_total_priority()}')
 
 
 if __name__ == '__main__':
