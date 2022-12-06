f44d3ba6df0759cc67015a1d56e3de6c92c856ce
Jordan Cook <jordan.cook.git@proton.me>
2022-12-05 23:30:35 -0600

    Problem 6, part 1

---
+++ solutions/problem_6.py
@@ -0,0 +1,13 @@
+from solutions import INPUTS_DIR
+
+
+def find_marker(message: str, marker_len: int) -> int:
+    for i in range(len(message)):
+        if len(set(message[i : i + marker_len])) == marker_len:
+            return i + marker_len
+
+
+if __name__ == '__main__':
+    with open(INPUTS_DIR / 'input_5') as fp:
+        message = fp.read()
+    print(f'Part 1: {find_marker(message, 4)}')
