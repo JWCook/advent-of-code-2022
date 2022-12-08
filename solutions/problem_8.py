b259003264471f25e7171045e47df0cbf4d92a9c
Jordan Cook <jordan.cook.git@proton.me>
2022-12-07 23:56:08 -0600

    Problem 8, part 2

--- solutions/problem_8.py
+++ solutions/problem_8.py
@@ -8,28 +8,59 @@ logger = getLogger(__name__)
 
 
 def parse_trees(data: str) -> int:
-    tree_grid = [[int(x) for x in line.strip()] for line in data.splitlines()]
+    return [[int(x) for x in line.strip()] for line in data.splitlines()]
+
+
+def count_visible_trees(tree_grid: list[list[int]]) -> int:
     logger.debug(tree_grid)
     grid_height = len(tree_grid)
     grid_width = len(tree_grid[0])
 
     def is_visible(x, y, tree_grid):
         tree_height = tree_grid[x][y]
-        top = [tree_grid[x][i] for i in range(min(y + 1, grid_height), grid_height)]
-        bot = [tree_grid[x][i] for i in range(0, y)]
-        left = [tree_grid[i][y] for i in range(min(x + 1, grid_width), grid_width)]
-        right = [tree_grid[i][y] for i in range(0, x)]
+        top = [tree_grid[x][i] for i in range(y + 1, grid_height)]
+        bot = [tree_grid[x][i] for i in range(y)]
+        left = [tree_grid[i][y] for i in range(x + 1, grid_width)]
+        right = [tree_grid[i][y] for i in range(x)]
         return any([all([i < tree_height for i in lst]) for lst in [top, bot, left, right]])
 
-    visible_trees = 0
-    for x in range(grid_width):
-        for y in range(grid_height):
-            if is_visible(x, y, tree_grid):
-                visible_trees += 1
-    return visible_trees
+    return sum(
+        [1 for x in range(grid_width) for y in range(grid_height) if is_visible(x, y, tree_grid)]
+    )
+
+
+def max_scenic_score(tree_grid: list[list[int]]) -> int:
+    grid_height = len(tree_grid)
+    grid_width = len(tree_grid[0])
+
+    def count_visible_trees_from(tree_height: int, lst: list[int]):
+        visible_trees = 0
+        for i in lst:
+            visible_trees += 1
+            if i >= tree_height:
+                break
+        return visible_trees
+
+    def get_scenic_score(x, y, tree_grid):
+        tree_height = tree_grid[x][y]
+        top = [tree_grid[x][i] for i in range(y + 1, grid_height)]
+        bot = [tree_grid[x][i] for i in reversed(range(y))]
+        left = [tree_grid[i][y] for i in range(x + 1, grid_width)]
+        right = [tree_grid[i][y] for i in reversed(range(x))]
+
+        scenic_score = 1
+        for lst in [top, bot, left, right]:
+            scenic_score *= count_visible_trees_from(tree_height, lst)
+        return scenic_score
+
+    return max(
+        [get_scenic_score(x, y, tree_grid) for x in range(grid_width) for y in range(grid_height)]
+    )
 
 
 if __name__ == '__main__':
     with open(INPUTS_DIR / 'input_8') as fp:
         data = fp.read()
-    logger.info(f'Part 1: {parse_trees(data)}')
+    tree_grid = parse_trees(data)
+    logger.info(f'Part 1: {count_visible_trees(tree_grid)}')
+    logger.info(f'Part 2: {max_scenic_score(tree_grid)}')
