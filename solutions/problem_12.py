07ecd5c5860b18704f434acc0739d585914af815
Jordan Cook <jordan.cook.git@proton.me>
2022-12-12 00:51:34 -0600

    Problem 12, part 2

--- solutions/problem_12.py
+++ solutions/problem_12.py
@@ -7,11 +7,14 @@ from solutions import read_input
 
 UNICODE_OFFSET = 96
 
+HeightMap = list[list[int]]
+
 
 def parse_map(data: str):
     heightmap = []
     start = None
     end = None
+
     for y, line in enumerate(data.splitlines()):
         heightmap_row = []
         for x, char in enumerate(line):
@@ -25,38 +28,41 @@ def parse_map(data: str):
                 height = ord(char) - UNICODE_OFFSET
             heightmap_row.append(height)
         heightmap.append(heightmap_row)
+
     return heightmap, start, end
 
 
-def format_map(heightmap, path=None):
+def format_map(heightmap: HeightMap, path=None):
+    def format_point(x, y, i):
+        return '**' if path and (x, y) in path else f'{i:>2}'
+
     return '\n'.join(
         [
-            ' '.join(['**' if path and (x, y) in path else f'{i:>2}' for x, i in enumerate(row)])
+            ' '.join([format_point(x, y, i) for x, i in enumerate(row)])
             for y, row in enumerate(heightmap)
         ]
     )
 
 
-def build_graph(heightmap: list[list[int]]):
+def build_graph(heightmap: HeightMap):
+    # Add nodes
     graph = nx.Graph().to_directed()
     for x in range(len(heightmap[0])):
         for y in range(len(heightmap)):
             graph.add_node((x, y))
 
+    # Add edges
     for x in range(len(heightmap[0])):
         for y in range(len(heightmap)):
             for (n_x, n_y) in get_neighbors(heightmap, x, y):
-                if (x, y) == (2, 2) and (n_x, n_y) == (3, 2):
-                    print('!!!!!!!!!!!!!!')
-                    print(x, y, n_x, n_y)
-                if (x, y) == (3, 2) and (n_x, n_y) == (2, 2):
-                    print('!##!!!!!!!!!!!!!')
-                    print(x, y, n_x, n_y)
                 graph.add_edge((x, y), (n_x, n_y))
+
+    logger.debug(list(graph.nodes))
+    logger.debug(list(graph.edges))
     return graph
 
 
-def get_neighbors(heightmap: list[list[int]], x: int, y: int):
+def get_neighbors(heightmap: HeightMap, x: int, y: int):
     neighbors = []
     if x > 0 and (heightmap[y][x - 1] - heightmap[y][x]) <= 1:
         neighbors.append((x - 1, y))
@@ -73,16 +79,37 @@ def shortest_path(graph, start, end):
     return nx.shortest_path(graph, start, end)
 
 
-if __name__ == '__main__':
-    data = read_input('12')
-
-    heightmap, start, end = parse_map(data)
-    logger.info('\n' + format_map(heightmap))
+def find_shortest_path(heightmap: HeightMap, start: int, end: int) -> int:
     graph = build_graph(heightmap)
     path = shortest_path(graph, start, end)
+    logger.debug(path)
+    logger.debug('\n' + format_map(heightmap))
     logger.info('\n' + format_map(heightmap, path))
-    # print(list(graph.nodes))
-    # print(list(graph.edges))
-    # print(path)
+    return len(path) - 1
 
-    logger.info(f'Part 1: \n{len(path)-1}')
+
+def find_shortest_path_from_any_start(heightmap: HeightMap, end: int) -> int:
+    starts = [
+        (x, y)
+        for x in range(len(heightmap[0]))
+        for y in range(len(heightmap))
+        if heightmap[y][x] == 1
+    ]
+    lengths = []
+
+    for start in starts:
+        graph = build_graph(heightmap)
+        try:
+            path = shortest_path(graph, start, end)
+        except:
+            pass
+        lengths.append(len(path) - 1)
+
+    return min(lengths)
+
+
+if __name__ == '__main__':
+    data = read_input(12)
+    heightmap, start, end = parse_map(data)
+    logger.info(f'Part 1: {find_shortest_path(heightmap, start, end)}')
+    logger.info(f'Part 2: {find_shortest_path_from_any_start(heightmap, end)}')
